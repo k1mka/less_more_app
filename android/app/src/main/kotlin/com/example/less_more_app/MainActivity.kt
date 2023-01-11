6362ad7cf5fa610e957de21fa6aa3858ad6fa4e1
georg <georgii.gabaidze@gmail.com>
2023-01-11 18:13:01 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/less_more_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.less_more_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:less_more_app/less_more_app.dart';
+
+void main() {
+  runApp(const LessMoreApp());
+}
+
+
